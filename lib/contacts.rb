a9fc7ab4af394c1fee9e3a61ba182260eff34cb0
Sierra Wallace <sskwallace@gmail.com>
2020-08-22 23:37:48 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,6 +1,18 @@
 require 'pry'
 
-puts 
+puts remove_strawberry(contacts)
+contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
